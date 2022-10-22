77a7e12537060920ad8a72efcbe15c5a8566658e
Joel Brownstein <joelbrownstein@sdss.org>
2022-10-22 16:42:30 -0600

    summary

--- python/datamodel_parser/textmining/Summary.py
+++ python/datamodel_parser/textmining/Summary.py
@@ -11,18 +11,32 @@ class Summary:
         self.set_hdu()
         self.set_header()
         self.set_keyword()
+        self.set_data()
+        self.set_column()
         
+    def set_column(self):
+        if self.data:
+            self.column = Column.query.filter(Column.data_id.in_([data.id for data in self.data]))
+            self.column = self.column.all()
+            if self.verbose: print("Summary> Found %r Columns" % len(self.column))
+
     def set_keyword(self):
         if self.header:
             self.keyword = Keyword.query.filter(Keyword.header_id.in_([header.id for header in self.header]))
             self.keyword = self.keyword.all()
-            if self.verbose: print("Summary> Found %r Headers" % len(self.header))
+            if self.verbose: print("Summary> Found %r Keywords" % len(self.keyword))
 
     def set_header(self):
         if self.hdu:
             self.header = Header.query.filter(Header.hdu_id.in_([hdu.id for hdu in self.hdu]))
             self.header = self.header.all()
             if self.verbose: print("Summary> Found %r Headers" % len(self.header))
+
+    def set_data(self):
+        if self.hdu:
+            self.data = Data.query.filter(Data.hdu_id.in_([hdu.id for hdu in self.hdu]))
+            self.data = self.data.all()
+            if self.verbose: print("Summary> Found %r Data rows" % len(self.data))
 
     def set_hdu(self):
         if self.file:
